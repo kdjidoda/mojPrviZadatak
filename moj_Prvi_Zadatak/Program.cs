639355b3616086bf6fc322f8c08e15b8ad4ef05c
Kevin Djidoda <118570461+kdjidoda@users.noreply.github.com>
2024-09-16 13:47:54 +0200

    Add files via upload

---
+++ moj_Prvi_Zadatak/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace moj_Prvi_Zadatak
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // deklaracija varijabli
+            // int a, b;
+
+            // definicija varijabli
+            int a = 100;
+            int b = 3;
+            int x = a / b;
+            int y = a % b;
+
+            // ispisivanje
+            string ispis = a + " dijeljeno s "
+                + b + " je " + x + " i ostatak " 
+                + y;
+            Console.WriteLine(ispis);
+
+            Console.ReadKey();
+        }
+    }
+}
